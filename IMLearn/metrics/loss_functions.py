5d718bd1ec6558e58921dcb18fdfe6954b0d5287
yuvalstn <yuval.steinberg1@mail.huji.ac.il>
2022-03-29 22:18:22 +0300

    implemented MSE function in metrics.loss_function

--- IMLearn/metrics/loss_functions.py
+++ IMLearn/metrics/loss_functions.py
@@ -16,7 +16,7 @@ def mean_square_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     -------
     MSE of given predictions
     """
-    raise NotImplementedError()
+    return np.sum(np.square(y_true-y_pred))/ (y_true.shape[0])
 
 
 def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: bool = True) -> float:
